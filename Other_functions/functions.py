215af7ec468e08f43df2543475cfc4a9ff9196fc
Maria Irudaya Regilan <mariairegilan@gmail.com>
2018-06-30 23:46:07 +0530

    First commit 30062018 night

--- Other_functions/functions.py
+++ Other_functions/functions.py
@@ -12,8 +12,7 @@ import numpy as np
 key=[77, 97, 114, 105, 97, 95, 73, 114, 117, 100, 97, 121, 97, 95, 82, 101, 103, 105, 108, 97, 110, 95, 108, 111, 118, 101, 115, 95, 78, 97, 110, 99, 121, 95, 83, 97, 98, 97, 116, 116, 105, 110, 105, 95, 115, 111, 95, 109, 117, 99, 104]
 
 #formatting the hash input to the list
-def getbest(ips):
-	cvdips=list(ips)
+def getbest(cvdips):
 	ips=[]
 	for i in cvdips:
 		ips.append(ord(i) if not str(i).isdigit() else i)
@@ -28,21 +27,11 @@ def as_text(nparray):
 	return t
 
 #hash function algorithm to encrypt the given text
-def doen(ips):
-	ips=getbest(ips)
+def doen(ips,state):
+	ips=getbest(list(ips)):
 	if len(ips) == len(key):
-		return as_text(np.array(ips)+np.array(key))
+		return as_text(np.array(ips)+np.array(key) if state else np.array(ips)-np.array(key))
 	elif len(ips)>len(key):
-		return as_text(np.array(ips[:len(key)])+np.array(key))+doen(ips[len(key):])
+		return as_text(np.array(ips[:len(key)])+np.array(key) if state else np.array(ips[:len(key)])-np.array(key))+doen(ips[len(key):],state)
 	else:
-		return as_text(np.array(ips)+np.array(key[:len(ips)]))
-
-#hash function algorithm to decrypt the given text
-def dode(ips):
-	ips=getbest(ips)
-	if len(ips) == len(key):
-		return as_text(np.array(ips)-np.array(key))
-	elif len(ips)>len(key):
-		return as_text(np.array(ips[:len(key)])-np.array(key))+dode(ips[len(key):])
-	else:
-		return as_text(np.array(ips)-np.array(key[:len(ips)]))
+		return as_text(np.array(ips)+np.array(key[:len(ips)]) if state else np.array(ips)-np.array(key[:len(ips)]))
--- main/views.py
+++ main/views.py
@@ -1,10 +1,18 @@
-#from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from Other_functions.functions import doen
+from django.contrib.auth import authenticate, login
 
-class MainPage(TemplateView):
-	template_name = "mainpage.html"
-
-	def get_context_data(self, *args, **kwargs):
-		context=super(MainPage,self).get_context_data(*args,**kwargs)
-		print(context)
-		return context
+def mainpageview(request):
+	if request.method=="POST":
+		form = auth_form(request.POST)
+		if form.is_valid():
+			user = authenticate(request, username=doen(form.cleaned_data.get('username'),True), password=doen(form.cleaned_data.get('password'),True))
+			if user is not None:
+				login(request, user)
+			else:
+				#another code
+		else:
+			#another code
+	else:
+		form = auth_form(request.GET)
+		return render(request,'mainpage',{'form':form})
